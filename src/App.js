348275f81825006197ea77954d3c8e1fac88c3f2
A-Hagag <ahmedhagag.work@gmail.com>
2025-08-13 17:26:49 +0300

    Interview task

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Search from "./components/Search";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
